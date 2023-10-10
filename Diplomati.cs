8f61abc99beaeff34ca4e9ec00af5ed20bdc5cce
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2023-10-10 20:28:18 +0200

    Aggiungere i file di progetto.

---
+++ Diplomati.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___Esercitazione_Diplomati
+{
+    internal class Diplomati
+    {
+        //Attributi
+        public string nome1;
+        public int voto1;
+
+        //Costruttore
+        public Diplomati (string nome1, int voto1,string[] array1,int cont1,string cond)
+        {
+            this.nome1 = nome1;
+            this.voto1 = voto1;
+
+            array1[cont1] = "nome: " + nome1 + ";" + voto1 + ";" + cond;
+        }
+
+        //Attributi
+        public void Stampa(string[] array1)
+        {
+            for(int i =0;i<array1.Length;i++)
+            {
+                if (array1[i] != null)
+                {
+                    Console.WriteLine(array1[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void Stampa2(string[] array1)
+        {
+            for(int i=0;i<array1.Length;i++)
+            {
+                if (array1[i] != null)
+                {
+                    string a = array1[i];
+                    string[] b = a.Split(';');
+
+                    if (b[2] == "sì")
+                    {
+                        if (Int32.Parse(b[1]) >= 70)
+                        {
+                            Console.WriteLine(a);
+                        }
+                    }
+                    else
+                    {
+                        if (Int32.Parse(b[1]) >= 42)
+                        {
+                            Console.WriteLine(a);
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using _5___Esercitazione_Diplomati;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _5_Esercitazione_Diplomati
+{
+    class program
+    {        
+
+        static void Main()
+        {
+            int scelta = 0;
+            int cont = 0;
+            string[] elenco = new string[100];
+            Diplomati a = new Diplomati("", 1, elenco, cont, ""); ;
+
+            //stuttura menù
+            do
+            {
+                //stampa delle opzioni
+
+                Console.Clear();
+                Console.WriteLine("5 - Esercitazione Diplomati");
+                Console.WriteLine(" ");
+                Console.WriteLine("          1 - Aggiunta di un diplomato: ");
+                Console.WriteLine("          2 - stampa di tutti i diplomati: ");
+                Console.WriteLine("          3 - stampa dei diplomati abili ai concorsi: ");
+                Console.WriteLine(" ");
+                Console.WriteLine("0 - uscita");
+                Console.WriteLine(" ");
+                //scelta dell'opzione
+                Console.WriteLine("inserisci la scelta ");
+                scelta = int.Parse(Console.ReadLine());
+
+
+                //esecuzione dell'opzione
+                switch (scelta)
+                {
+                    case 1:
+
+
+                        Console.WriteLine("Inserisci nome diplomato: ");
+                        string nome = Console.ReadLine();
+
+                        Console.WriteLine("E' un nuovo diplomato: ");
+                        string cond = Console.ReadLine();
+
+                        int voto = 0;
+
+                        if (cond == "sì")
+                        {
+                            Console.WriteLine("Inserisci punteggio tra 60 e 100: ");
+                            voto = int.Parse(Console.ReadLine());                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Inserisci punteggio tra 36 e 60: ");
+                            voto = int.Parse(Console.ReadLine());
+                        }
+
+                        a = new Diplomati(nome, voto, elenco, cont,cond);
+                        
+                        cont++;
+
+                        break;
+
+                        case 2:
+
+                        a.Stampa(elenco);
+
+                        break;
+
+                        case 3:
+
+                        a.Stampa2(elenco);
+
+                        break;
+                }
+
+                Console.ReadLine();
+            } while (scelta != 0);
+        }   
+    }
+
+}
